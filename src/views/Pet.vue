a14099bf211a238ee837031a2f8d7d3544fd699e
Jonnyy <caijishiwo@gmail.com>
2020-03-26 23:11:24 -0500

    rebuilt components

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,3 +1,31 @@
 <template>
-    <div>Pet</div>
+    <div>
+        <h1>{{ animal.name }} ({{ $route.params.species }})</h1>
+        <p>{{ animal.breed }}</p>
+        <p>{{ animal.gender }}</p>
+       
+    </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data() {
+    return {
+        animal: {}
+    };
+  },
+  computed:{
+      ...mapState([
+          'cats',
+           'dog',
+      ])
+  },
+   mounted(){
+      
+          const animal = this[this.$route.params.species][this.$route.params.id]
+          this.animal = animal
+          
+  }
+};
+</script>
