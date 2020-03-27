2a38fadc787ffe0599bdbe45c366d094821dbf03
Jonnyy <caijishiwo@gmail.com>
2020-03-26 23:10:14 -0500

    add cats data

---
+++ src/data/cats.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: '1',
+        breed: '2',
+        gender: '3'
+    },
+    {
+        name: '2',
+        breed: '3',
+        gender: '4'
+    },
+    {
+        name: '3',
+        breed:'2',
+        gender: '3'
+    },
+    {
+        name: '4',
+        breed: '2',
+        gender: '3'
+    }
+]
